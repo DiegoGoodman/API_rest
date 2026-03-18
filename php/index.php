0a54626995c6c83fe3f59d9d647e871cda3c86f6
student <student@itismeucci.com>
2026-03-18 12:12:15 +0100

    modifica put

--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@ use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
